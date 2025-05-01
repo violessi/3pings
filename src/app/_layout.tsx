cd5c9a3a7922719d015cf80755b42762f91f42d6
violessi <108144748+violessi@users.noreply.github.com>
2025-05-02 03:06:04 +0800

    feat: use react native paper in app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,17 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 import "@/src/utils/global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="index" options={{ title: 'Home' }} />
+    <PaperProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="index" options={{ title: 'Home' }} />
       <Stack.Screen name="about" options={{ title: 'About' }} /> */}
-    </Stack>
+      </Stack>
+    </PaperProvider>
   );
 }
 
