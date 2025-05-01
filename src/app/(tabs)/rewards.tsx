3470addf269f614596b2cc08d248d1d0b9ac9481
violessi <108144748+violessi@users.noreply.github.com>
2025-05-02 00:22:08 +0800

    feat: move app into a folder

--- src/app/(tabs)/rewards.tsx
+++ src/app/(tabs)/rewards.tsx
@@ -1,5 +1,5 @@
-import { Text, View, StyleSheet } from 'react-native';
-import globalStyles from '../../assets/styles';
+import { Text, View, StyleSheet } from "react-native";
+import globalStyles from "../../../assets/styles";
 
 export default function RewardsScreen() {
   return (
