ab8321aaf71e5c0256993ed58ea04afd19c6c60f
violessi <108144748+violessi@users.noreply.github.com>
2025-05-13 18:42:44 +0800

    chore: cleanup

--- server/server.js
+++ server/server.js
@@ -5,15 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/*
 app.get('/', (req, res) => {
   res.send('Welcome to the Express server!');
 });
-
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
-});
-*/
 
 // Load routes
 app.use('/api/bikeActions', require('./routes/bikeActions'));
